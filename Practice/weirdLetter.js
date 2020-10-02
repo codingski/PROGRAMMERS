09cbf130882b6f52fd0020a80cc9b5ee1296743a
Coding_Chan <69722812+codingski@users.noreply.github.com>
2020-10-02 23:05:15 +0900

    미완성
    2020. 10. 02
    
    미완성 ㅠㅠ

---
+++ Practice/weirdLetter.js
@@ -0,0 +1,14 @@
+function solution(s){
+    var array = s.split(' ');
+    var j = 0;
+    for(var i = 0; i < array.length; i++){
+        for(j = 0 ; j < array[i].length / 2; j++){
+            array = array[i].replace(array[i][j * 2], array[i][j * 2].toUpperCase());
+        }
+    }
+
+    return array;
+}
+
+
+console.log("retre ertrefd gdfg".split(' '));
