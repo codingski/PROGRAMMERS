6a4e07741a5caf36a7c76b6c21bbcf61cd677b7d
Coding_Chan <69722812+codingski@users.noreply.github.com>
2020-10-05 23:50:48 +0900

    Update weirdLetter.js

--- Practice/weirdLetter.js
+++ Practice/weirdLetter.js
@@ -1,13 +1,35 @@
+/*
 function solution(s){
     var array = s.split(' ');
     var j = 0;
-    for(var i = 0; i < array.length; i++){
+    var i = 0;
+    for(i = 0; i < array.length; i++){
         for(j = 0 ; j < array[i].length / 2; j++){
             array = array[i].replace(array[i][j * 2], array[i][j * 2].toUpperCase());
         }
     }
+
     return array;
 }
 
 
-console.log("retre ertrefd gdfg".split(' '));
+console.log(solution('adsd asdasd asdas'));
+*/
+var sum = '';
+var ss = 'try hello world';
+var i = 0;
+var array = ss.split(' ');
+var number = array.length;
+while(ss[i] != ' '){
+    if(i % 2 === 0){
+        sum += ss[i].toUpperCase();
+    }
+    else{
+        sum += ss[i];
+    }
+    i++;
+}
+
+console.log(sum);
+
+AaAa AaAa AaA AaAa
