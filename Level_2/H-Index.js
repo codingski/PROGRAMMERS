2e2b5de481de3164ef85ebc63d2efd38bb18191b
Coding_Chan <69722812+codingski@users.noreply.github.com>
2020-10-14 22:25:14 +0900

    2020. 10. 14
    2020. 10. 14
    
    https://programmers.co.kr/learn/courses/30/lessons/42747

---
+++ Level_2/H-Index.js
@@ -0,0 +1,51 @@
+/*
+    문제 설명
+    H-Index는 과학자의 생산성과 영향력을 나타내는 지표입니다. 어느 과학자의 H-Index를 나타내는 값인 h를 구하려고 합니다. 위키백과1에 따르면, H-Index는 다음과 같이 구합니다.
+
+    어떤 과학자가 발표한 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index입니다.
+
+    어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
+
+    제한사항
+    과학자가 발표한 논문의 수는 1편 이상 1,000편 이하입니다.
+    논문별 인용 횟수는 0회 이상 10,000회 이하입니다.
+
+    입출력 예)
+    citations -> [3, 0, 6, 1, 5]
+    return -> 3
+*/
+
+// My code
+function solution(citations){
+    let temp = [];
+    let count = 0;
+    let j = 0;
+    let Citations = citations.sort(function(a, b){return a - b});
+    for(let i = 0; i < Citations[Citations.length - 1]; i++){
+        temp.push(Citations.filter(function(a){return i <= a}).length);
+    }
+    while(j < temp[j]){
+        j++;
+        count++;
+        
+    }
+    return count;
+}
+
+console.log(solution([3, 0, 6, 1, 5]));
+
+// Best code
+
+function solution(citations) {
+     citations = citations.sort(sorting);
+     var i = 0;
+     while(i + 1 <= citations[i]){
+         i++;
+     }
+     return i;
+
+
+     function sorting(a, b){
+         return b - a;
+     }
+}
