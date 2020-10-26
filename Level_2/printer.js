2d23e3fffd624f50a449b7c50c70546f9e30b972
Coding_Chan <69722812+codingski@users.noreply.github.com>
2020-10-26 22:31:00 +0900

    2020. 10. 26 미완성

---
+++ Level_2/printer.js
@@ -0,0 +1,35 @@
+function solution(priorities, location){
+    let array = priorities;
+    let Location = location;
+    for(let i = 0; i < array.length; i++){
+        let count = i + 1;
+        let count2 = 0;
+        while(true){
+            if(array[i] < array[count]){
+                if(Location === 0){
+                    Location = array.length - 1;
+                }
+                else if(Location === i){
+                    Location = array.length - 1;
+                }
+                else{
+                    Location = Location - 1;
+                }
+                array.push(array[i]);
+                array.splice(i, 1);
+                count = i + 1;
+            }
+            else{
+                count++;
+            }
+            count2++;
+            if(count2 === 100){
+                break;
+            }
+        }
+    }
+    return Location + 1;
+}
+
+console.log(solution([1, 2, 3, 4, 1], 0));
+
